fd7f1befe9596335ced1e029083820b87cd8a138
Hiago Silva <hiagosilvaanjos7@gmail.com>
2022-02-08 14:55:06 -0300

    Atualizei o style e criei o botão clear

--- javascript/script.js
+++ javascript/script.js
@@ -1,8 +1,8 @@
 const inputElement = document.querySelector('.new-task-input');
 const addTaskButton = document.querySelector('.new-task-button');
+const clearTasksElement = document.querySelector('.btn-clear')
 
 const tasksContainer = document.querySelector('.task-list');
-
 
 // VALIDAÇÃO DO INPUT
 
@@ -49,6 +49,12 @@ const handleAddTask = () => {
     // Adicionando a div taskItemContainer em tasksContainer
     tasksContainer.appendChild(taskItemContainer);
  
+    // Se a div tasksContainer tiver uma tarefa o botão clear aparece
+    if (tasksContainer.contains(taskItemContainer)) {
+      clearTasksElement.style.display = 'block'
+    }
+    
+
     // Após a tarefa ser criada o campo input vai ser limpo
     inputElement.value = "";
 
@@ -66,6 +72,9 @@ const handleAddTask = () => {
     passando como parâmetro o próprio elemento clicado
     */
     deleteItem.addEventListener('click', () => handleDeleteClick(taskItemContainer, taskContent));
+
+    // Excluir todas as tarefas
+    clearTasksElement.addEventListener('click', () => handleClearTasksElement())
 
     // Atualizar local storage
     updateLocalStorage();
@@ -106,10 +115,28 @@ const handleDeleteClick = (taskItemContainer, taskContent) => {
         }
     }
 
+    // Se não tiver nenhuma tarefa, o botão clear vai sumir
+    if(tasksContainer.innerHTML == '') {
+      clearTasksElement.style.display = 'none'
+    }
+
     updateLocalStorage();
 }
 
+/*
+A função handleClearTasksElement vai ter uma constante que vai receber todos os elementos filhos de tasksContainer (todas as tarefas adicionadas)
+E para cada tarefa recebida no loop, será removida
+*/
+const handleClearTasksElement = () => {
+  const tasks = tasksContainer.childNodes;
+  
+    for (const task of tasks) {
+        task.remove()
+        clearTasksElement.style.display = 'none'
+    }
 
+    updateLocalStorage();
+}
 
 // Remover mensagem de erro
 const handleInputChange = () => {
@@ -129,13 +156,11 @@ const updateLocalStorage = () => {
     
       return { description: content.innerText, isCompleted };
     });
-    console.log(JSON.stringify(localStorageTasks))
     localStorage.setItem("tasks", JSON.stringify(localStorageTasks));
   };
   
   const refreshTasksUsingLocalStorage = () => {
     const tasksFromLocalStorage = JSON.parse(localStorage.getItem("tasks"));
-    console.log(tasksFromLocalStorage)
 
     if (!tasksFromLocalStorage) return;
   
@@ -159,12 +184,19 @@ const updateLocalStorage = () => {
       taskItemContainer.appendChild(deleteItem);
       
       tasksContainer.appendChild(taskItemContainer);
+
+      if (tasksContainer.contains(taskItemContainer)) {
+        clearTasksElement.style.display = 'block'
+      }
       
       taskContent.addEventListener("click", () => handleClick(taskContent));
 
       deleteItem.addEventListener("click", () =>
         handleDeleteClick(taskItemContainer, taskContent)
       );
+
+      clearTasksElement.addEventListener('click', () => 
+      handleClearTasksElement(taskItemContainer, taskContent))
     }
   };
   
